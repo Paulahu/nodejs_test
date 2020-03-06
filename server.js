1b994c6160c5477e58d769e701570b92aed3121a
Paulahu <paulahu1995@gmail.com>
2020-03-06 14:39:48 +0800

    demo_test

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ var server = http.createServer(function(request, response) {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
     response.write(`
-        <link rel="stylesheet" href="./x">
+        <link rel="stylesheet" href="./style.css">
         <h1>你好</h1>`);
     response.end();
-  } else if (path === "/x") {
+  } else if (path === "/style.css") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/css;charset=utf-8");
     response.write(`h1{color: red;}`);
@@ -36,7 +36,7 @@ var server = http.createServer(function(request, response) {
   } else {
     response.statusCode = 404;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`你输入的路径不存在对应的内容`);
+    response.write(`你访问的页面不存在`);
     response.end();
   }
 });
